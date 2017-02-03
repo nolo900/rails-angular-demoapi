f8d9ee909ae071bd8f93d5bdb0e265718e8180f7
Austin Miles <abm@austinbmiles.com>
2017-02-03 16:19:34 -0500

    fix patient in migrate file

--- db/migrate/20170203193647_create_patients.rb
+++ db/migrate/20170203193647_create_patients.rb
@@ -2,7 +2,7 @@ class CreatePatients < ActiveRecord::Migration[5.0]
   def change
     create_table :patients do |t|
       t.string :name
-      t.string :insurance
+      t.string :insurance_co
       t.string :gender
       t.boolean :new_patient
 
